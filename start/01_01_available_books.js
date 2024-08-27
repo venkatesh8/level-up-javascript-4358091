ea51239c0531acc6d91940952416496095fa7c1f
Venkatesh B <venky88.iiitb@gmail.com>
2024-08-27 05:37:18 +0000

    simple change

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,39 @@
 // Write your code here
+class Book{
+  constructor(title, author, ISBN, numCopies){
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies
+  }
+
+  getAvalilability(){
+    if(this.numCopies === 0){
+      return "Out of Stock"
+    }else if(this.numCopies <10){
+      return "Restocking needed"
+    }else{
+      return "In Stock"
+    }
+  }
+
+  get status(){
+    return this.getAvalilability()
+  }
+
+  sell(numCopiesSold = 1){
+    this.numCopies -= numCopiesSold
+  }
+
+  restock(numCopiesRestocked = 5){
+    this.numCopies += numCopiesRestocked
+  }
+}
+
+const HungerGames = new Book('Hunger Games', 'Venkatesh Badiger', 124122, 4)
+
+HungerGames.sell(17)
+HungerGames.getAvalilability()
+
+HungerGames.restock(17)
+HungerGames.getAvalilability()
